1442f35a0fc35cae45a84c7bcded72059a5195de
Ayaz Ahmed Memon <ayazahmed313@gmail.com>
2024-02-02 03:35:02 +0500

    create asp.net core api project & postgres sql with entity framework

---
+++ ProfileSync.API/Data/ApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProfileSync.API.Models;
+
+namespace ProfileSync.API.Data;
+
+public class ApiDbContext: DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //optionsBuilder.UseSqlServer(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=ContosoPizza-Part1;Integrated Security=True;Encrypt=False");
+        optionsBuilder.UseNpgsql("User ID=postgres;Password=1234;Host=localhost;Port=5432;Database=ProfileSyncApp;");
+    }
+}
---
+++ ProfileSync.API/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfileSync.API.Models;
+
+public class User
+{
+    [Key]
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+}
