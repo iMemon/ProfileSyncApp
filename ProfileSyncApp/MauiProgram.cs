07d2dff8e37747f8387908e0678edebd57f8ec1b
Ayaz Ahmed Memon <ayazahmed313@gmail.com>
2024-02-15 01:13:00 +0500

    setup on vs-code on mac (android)

--- ProfileSyncApp/MauiProgram.cs
+++ ProfileSyncApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace ProfileSyncApp
 {
@@ -9,6 +10,7 @@ namespace ProfileSyncApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
